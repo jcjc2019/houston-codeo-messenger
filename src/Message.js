907f4dfabe58be87ae80b55d66990148a7c2a78b
JingChen <jingchenjc2019@gmail.com>
2019-04-11 13:22:27 -0500

    completed.

--- src/Message.js
+++ src/Message.js
@@ -2,12 +2,11 @@
 class Message{
 
     constructor(content){
-        this.content = content
-        // this.element = document.createElement('li')
+        this.content = content;
     }
 
     renderMessage(){
-        let ul = document.querySelector('#messages')
+        let ul = document.querySelector('#messages');
         let li = document.createElement('li');
         li.innerText = this.content;
         ul.append(li);
